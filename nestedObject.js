d1fb4a207ae9916ba5005f39b4f8fdbb15394640
regycloud <regy888@gmail.com>
2021-04-05 00:00:13 +0700

    Added another file

---
+++ nestedObject.js
@@ -0,0 +1,71 @@
+let spaceship = {
+    passengers: null,
+    telescope: {
+      yearBuilt: 2018,
+      model: "91031-XLT",
+      focalLength: 2032 
+    },
+    crew: {
+      captain: { 
+        name: 'Sandra', 
+        degree: 'Computer Engineering', 
+        encourageTeam() { console.log('We got this!') },
+       'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+    },
+    engine: {
+      model: "Nimbus2000"
+    },
+    nanoelectronics: {
+      computer: {
+        terabytes: 100,
+        monitors: "HD"
+      },
+      'back-up': {
+        battery: "Lithium",
+        terabytes: 50
+      }
+    }
+  }; 
+//   assign variable capFave with caption the most favorite food from spaceship object.
+const capFave = spaceship.crew.captain['favorite foods'][0];
+
+// Instead, assign as its value an array of objects. These objects should represent the spaceship‘s passengers as individual objects. Make at least one passenger object in the array that has at least one key-value pair on it.
+spaceship.passengers = [{name: 'John'}];
+
+// Create a variable firstPassenger and assign the first passenger as its value (the element in the 0th index of the spaceship.passengers array you just made). Make sure to use bracket and dot notation to get the passenger object through nested access (don’t just copy the object into the variable!)
+const firstPassenger = spaceship.passengers[0];
+
+
+
+// let spaceship = {
+//     crew: {
+//     captain: { 
+//         name: 'Lily', 
+//         degree: 'Computer Engineering', 
+//         cheerTeam() { console.log('You got this!') } 
+//         },
+//     'chief officer': { 
+//         name: 'Dan', 
+//         degree: 'Aerospace Engineering', 
+//         agree() { console.log('I agree, captain!') } 
+//         },
+//     medic: { 
+//         name: 'Clementine', 
+//         degree: 'Physics', 
+//         announce() { console.log(`Jets on!`) } },
+//     translator: {
+//         name: 'Shauna', 
+//         degree: 'Conservation Science', 
+//         powerFuel() { console.log('The tank is full!') } 
+//         }
+//     }
+// }; 
+
+// // Write your code below
+// for (const crew in spaceship.crew) {
+//   // console.log(`${crew}: ${spaceship.crew[crew].name}`);
+//   console.log(`${spaceship.crew[crew].name}: ${spaceship.crew[crew].degree}`)
+// }
+
+let arr = [0, 'satu', 2, 'tiga'];
+console.log(arr);
