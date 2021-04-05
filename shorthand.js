f56c313fd942e20feeaaaf62decf4bf4776c5dd2
regycloud <regy888@gmail.com>
2021-04-05 19:35:17 +0700

    Add shorthand verson of function and object

---
+++ shorthand.js
@@ -0,0 +1,25 @@
+// Shorthand version
+const robotFactory = (model, mobile) => {
+    return {
+      model,
+      mobile,
+      beep() {
+        console.log('Beep Boop');
+      }
+    }
+  }
+  
+  // function robotFactory(model, mobile){
+  //   return {
+  //     model: model,
+  //     mobile: mobile,
+  //     beep() {
+  //       console.log('Beep Boop');
+  //     }
+  //   }
+  // }
+  
+  // To check that the property value shorthand technique worked:
+  const newRobot = robotFactory('P-501', false)
+  console.log(newRobot.model)
+  console.log(newRobot.mobile)
