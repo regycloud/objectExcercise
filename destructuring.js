5e52125dc98f18e58ee4f70d62bfa9ba64b02f6a
regycloud <regy888@gmail.com>
2021-04-05 19:51:13 +0700

    added destructuring

---
+++ destructuring.js
@@ -0,0 +1,17 @@
+const robot = {
+    model: '1E78V2',
+    energyLevel: 100,
+    functionality: {
+      beep() {
+        console.log('Beep Boop');
+      },
+      fireLaser() {
+        console.log('Pew Pew');
+      },
+    }
+};
+//  Use destructured assignment to create a const variable named functionality that extracts the functionality property of robot. 
+  const {functionality} = robot;
+
+// Since functionality is referencing robot.functionality we can call the methods available to robot.functionality simply through functionality. Take advantage of this shortcut and call the .beep() method on functionality.
+  functionality.beep();
