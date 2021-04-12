c6e0597d9431fcd8137060db15e03d82938e6125
regycloud <regy888@gmail.com>
2021-04-12 22:55:14 +0700

    Added forEach Method

---
+++ forEachMethod.js
@@ -0,0 +1,12 @@
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach(function(fruitItem) {
+  let article = '';
+  if (fruitItem[0] == 'a' || fruitItem[0] == 'i' || fruitItem[0] == 'u' || fruitItem[0] == 'e' || fruitItem[0] == 'o') {
+    article = 'an';
+  } else {
+    article = 'a';
+  }
+  console.log(`I want to eat ${article} ${fruitItem}`)
+});
