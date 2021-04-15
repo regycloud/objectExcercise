20a8fda82709939ec3aa6081048d4671f4498a14
regycloud <regy888@gmail.com>
2021-04-15 22:36:51 +0700

    Add every Method excercise method

---
+++ everyMethod.js
@@ -0,0 +1,5 @@
+// The every() method tests whether all elements in the array pass the test implemented by the provided function. It returns a Boolean value.
+
+const animals = ['cat', 'fish', 'dog', 'ant'];
+
+console.log(animals.every(animal => animal.length >= 3));
